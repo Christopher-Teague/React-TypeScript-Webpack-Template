5693225692283ea7286ba5f1bf6b681ab775f69b
Christopher-Teague <cmteague77@gmail.com>
2022-06-22 18:29:42 -0700

    debug

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ import { ClickCounter } from './ClickCounter'
 export const App = () => {
   return (
     <>
-      <h1>React TypeScript Webpack Template</h1>
+      <h1>Testing React TypeScript Webpack Template</h1>
 
       <ClickCounter />
     </>
